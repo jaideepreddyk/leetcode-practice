fa02a9299e285e1e57c6d1053b737e64b7ffbf2a
Jaideep Reddy Kommera <42896931+jaideepreddyk@users.noreply.github.com>
2023-12-25 23:26:13 -0500

    alternate string -slightly faster implementation

--- AlternatingString.java
+++ AlternatingString.java
@@ -3,28 +3,32 @@ import java.util.Scanner;
 public class AlternatingString {
     public static int minOperations(String s) {
         if(s.length()==1) return 0;
-        char[] str_arr = s.toCharArray();
+        char[] str_arr = s.toCharArray();        
         if(s.length()==2){
             if(str_arr[0]==str_arr[1]) return 1;
         }
-        // creating solutions
-        int arr_len = s.length()+(s.length()%2);
-        String sol_1, sol_2;
-        sol_1 = sol_2 = "";
 
-        for(int i =0; i<arr_len/2;i++){
-            sol_1 = sol_1 + "10";
-            sol_2 = sol_2 + "01";
+        Boolean bool = true;
+        int[] input_arr = new int[s.length()];
+
+        // creating solutions
+        int[] sol1_arr = new int[s.length()];
+        int[] sol2_arr = new int[s.length()];
+
+        for(int i=0; i<input_arr.length; i++){
+            bool = !bool;
+            input_arr[i] = Character.getNumericValue(str_arr[i]);
+
+            sol1_arr[i] = bool?1:0;
+            sol2_arr[i] = !bool?1:0;
         }
 
-        char[] sol1_arr = sol_1.toCharArray();
-        char[] sol2_arr = sol_2.toCharArray();
         
         int counter1, counter2;
         counter1 = counter2 = 0;
-        for(int i = 0; i<str_arr.length; i++){
-            if(str_arr[i]!=sol1_arr[i]) counter1++;
-            else if(str_arr[i]!=sol2_arr[i]) counter2++;
+        for(int i = 0; i<input_arr.length; i++){
+            if(input_arr[i]!=sol1_arr[i]) counter1++;
+            else if(input_arr[i]!=sol2_arr[i]) counter2++;
         }
         return Math.min(counter1, counter2);
     }
