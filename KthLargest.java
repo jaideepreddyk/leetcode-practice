59e87ea75e332c40d8cb4b4cfe1afd1d6a827e54
Jaideep Reddy Kommera <42896931+jaideepreddyk@users.noreply.github.com>
2024-03-31 23:34:31 -0400

    did it with minHeap instead - got top 99%

--- KthLargest.java
+++ KthLargest.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 
 public class KthLargest {
 
-    public class MaxHeap{
+    public class MinHeap{
         int[] heap;
         int capacity = 0;
-
-        MaxHeap(){
-            heap = new int[10000];
+        int k;
+        MinHeap(int k){
+            this.k = k;
+            this.heap = new int[k+1];
         }
 
         public int parent(int index){
@@ -23,16 +24,23 @@ public class KthLargest {
         }
 
         public void insert(int num){
-            this.capacity++;
-            heap[capacity] = num;
-            siftUp(capacity);
+            if(this.capacity<k){
+                this.capacity++;
+                heap[capacity] = num;
+                siftUp(capacity);
+            }
+            else if(this.heap[1]<num){
+                getMin();
+                insert(num);
+            }
+            return;
         }
 
         public void siftUp(int index){
             if(parent(index)<1) return;
             int parent_val = heap[parent(index)];
             int curr_val = heap[index];
-            if(parent_val<curr_val){
+            if(parent_val>curr_val){
                 heap[index] = parent_val;
                 heap[parent(index)] = curr_val;
                 siftUp(parent(index));
@@ -41,58 +49,49 @@ public class KthLargest {
         }
 
         public void siftDown(int index){
-            int maxIndex = index;
+            int minIndex = index;
             int leftIdx = leftChild(index);
             int rightIdx = rightChild(index);
-            if(leftIdx<=this.capacity && heap[maxIndex]<heap[leftIdx]){
-                maxIndex = leftIdx;
+            if(leftIdx<=this.capacity && heap[minIndex]>heap[leftIdx]){
+                minIndex = leftIdx;
             }
-            if(rightIdx<=this.capacity && heap[maxIndex]<heap[rightIdx]){
-                maxIndex = rightIdx;
+            if(rightIdx<=this.capacity && heap[minIndex]>heap[rightIdx]){
+                minIndex = rightIdx;
             }
-            if(maxIndex==index){
+            if(minIndex==index){
                 return;
             }
             else{
                 //swap elements
-                int temp = heap[maxIndex];
-                heap[maxIndex]= heap[index];
+                int temp = heap[minIndex];
+                heap[minIndex]= heap[index];
                 heap[index] = temp;
-                siftDown(maxIndex);
+                siftDown(minIndex);
             }
             return;
         }
 
-        public int getMax(){
-            int max = heap[1];
+        public int getMin(){
+            int min = heap[1];
             heap[1] = heap[capacity];
             this.capacity--;
             siftDown(1);
-            return max;
+            return min;
         }
     }
 
-    KthLargest.MaxHeap myheap;
-    int k;
+    KthLargest.MinHeap myheap;
     public KthLargest(int k, int[] nums) {
-        this.myheap = new MaxHeap();
-        this.k = k;
-        for(int num : nums){
-            this.myheap.insert(num);
+        this.myheap = new MinHeap(k);
+        if(nums.length>0){
+            for(int num : nums){
+                this.myheap.insert(num);
+            }
         }
     }
     
     public int add(int val) {
         this.myheap.insert(val);
-        int kmax = 0;
-        int[] reinsert = new int[k];
-        for(int i=0; i<this.k;i++){
-            kmax = this.myheap.getMax();
-            reinsert[i] = kmax;
-        }
-        for(int i : reinsert){
-            this.myheap.insert(i);
-        }
-        return kmax;
+        return this.myheap.heap[1];
     }
 }
